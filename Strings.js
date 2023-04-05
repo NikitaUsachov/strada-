da05bece332145fb5b9265f00a1b62b08c0cf065
Nikita <usa-nikita@yandex.ru>
2023-04-05 13:15:52 +0300

    rewritten using 'slice'

--- Strings.js
+++ Strings.js
@@ -2,21 +2,16 @@ function vertLine(word) {
     for (let char = 0; char < word.length; char++) {
         if (word.length >= 7 && word[0] === "s"){
             console.log(word[0].toUpperCase())
-            console.log(word[1])
-            console.log(word[2])
-            console.log(word[3])
-            console.log(word[4])
-            console.log(word[5])
-            console.log(word[6])
+            for (let i = 1; i <= word.slice(1).length;i++){
+                console.log(word.charAt(i))
+            }
             break;
         }
         else if (word[0] === "s"){
             console.log(word[0].toUpperCase())
-            console.log(word[1])
-            console.log(word[2])
-            console.log(word[3])
-            console.log(word[4])
-            console.log(word[5])
+            for (let i = 1; i <= word.slice(1).length;i++){
+                console.log(word.charAt(i))
+            }
             break;
         }
         else {
